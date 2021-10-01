725365bc4dbb8e0b254b4f02808e857096d6bb1c
Dabizu <dabizu100@gmail.com>
2021-10-01 04:31:40 -0500

    se reacomodo el cors

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app=express();
 const cors = require("cors");
-app.use(cors());
 const port = process.env.PORT || 3000
 const MongoClient = require('mongodb').MongoClient;
 const base = "base";
@@ -19,6 +18,7 @@ const url = "mongodb+srv://"+user+":"+password+"@cluster0.s9ejk.mongodb.net/"+ba
       db.close();
     });
 });*/
+app.use(cors());
 app.get("/",(req,res)=>{
     //res.sendFile(__dirname+"/login.html");
     res.send("hola");
